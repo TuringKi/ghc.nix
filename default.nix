8d533cb45586c14249c15592ba2ec67357bfb52c
mxlol233 <turingki@yeah.net>
2022-08-10 23:44:16 +0800

    Update default.nix

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@ let
   sources = import ./nix/sources.nix {};
 in
 { nixpkgs   ? import (sources.nixpkgs) {}
-, bootghc   ? "ghc865"
+, bootghc   ? "ghc902"
 , version   ? "9.3"
 , hadrianCabal ? (builtins.getEnv "PWD") + "/hadrian/hadrian.cabal"
 , nixpkgs-unstable ? import (sources.nixpkgs-unstable) {}
